a1a0f9f62ac3ca5e670c2c162651dc49eaa17203
Benoit Tachet <benoit.tachet@epitech.eu>
2019-09-20 16:58:44 +0200

    Ajout d'un ecran titre

--- GD/Assets/Jimmy/Player.cs
+++ GD/Assets/Jimmy/Player.cs
@@ -1,20 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     Rigidbody playerRb = null;
     public Camera camera;
-
-    public float movementSpeed = 4.0f;
+	public float movementSpeed = 4.0f;
     public int tireingLevel = 0;
     public int food = 0;
-    private Vector3 offset;
+    public Button startButton;
+	
+	private Vector3 offset;
 
     // Start is called before the first frame update
     void Start()
     {
+		//On ne fait pas bouger le player tant que l'ecran titre est affiche
+		
         playerRb = GetComponent<Rigidbody>();
         offset = camera.transform.position - transform.position;
     }
@@ -23,22 +27,25 @@ public class Player : MonoBehaviour
     void Update()
     {
         camera.transform.position = transform.position + offset;
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            playerRb.velocity = new Vector3(1, 0, 0) * movementSpeed;
-        }
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            playerRb.velocity = new Vector3(-1, 0, 0) * movementSpeed;
-        }
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            playerRb.velocity = new Vector3(0, 0, 1) * movementSpeed;
-        }
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            playerRb.velocity = new Vector3(0, 0, -1) * movementSpeed;
-        }
+        if (!startButton.gameObject.activeSelf)
+		{
+			if (Input.GetKey(KeyCode.RightArrow))
+			{
+				playerRb.velocity = new Vector3(1, 0, 0) * movementSpeed;
+			}
+			if (Input.GetKey(KeyCode.LeftArrow))
+			{
+				playerRb.velocity = new Vector3(-1, 0, 0) * movementSpeed;
+			}
+			if (Input.GetKey(KeyCode.UpArrow))
+			{
+				playerRb.velocity = new Vector3(0, 0, 1) * movementSpeed;
+			}
+			if (Input.GetKey(KeyCode.DownArrow))
+			{
+				playerRb.velocity = new Vector3(0, 0, -1) * movementSpeed;
+			}
+		}
     }
 
 	void OnTriggerEnter(Collider other)

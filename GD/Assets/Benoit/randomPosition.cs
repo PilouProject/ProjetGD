e9e841f401401cbdbfdee19b7c56310fbb0b8fa1
Benoit Tachet <benoit.tachet@epitech.eu>
2019-09-20 15:38:24 +0200

    Mouvements + Collisions avec les pickables

---
+++ GD/Assets/Benoit/randomPosition.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class randomPosition : MonoBehaviour
+{
+	
+	public float x, z;
+	float y = 0.5f;
+	Vector3 pos;
+	
+    void Start()
+    {
+        x = Random.Range(-10, 10);
+		z = Random.Range(-10, 10);
+		pos = new Vector3(x, y, z);
+		transform.position = pos;
+    }
+
+    void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("Wall"))
+		{
+			x = Random.Range(-10, 10);
+			z = Random.Range(-10, 10);
+			pos = new Vector3(x, y, z);
+			transform.position = pos;
+		}
+	}
+}

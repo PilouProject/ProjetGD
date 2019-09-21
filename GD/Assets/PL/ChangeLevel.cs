00d29bdecd235bb8017885a322db113b631499c5
Pilou <pierre-louis.franques@epitech.eu>
2019-09-21 11:43:57 +0200

    Fade black scene

---
+++ GD/Assets/PL/ChangeLevel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeLevel : MonoBehaviour
+{
+    public bool _trigger;
+
+    // Update is called once per frame
+    void Start()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "House")
+            _trigger = true;
+    }
+}
+

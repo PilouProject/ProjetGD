b234a1d2874f6a491e3f1c7784f6e8fafc67b397
Pilou <pierre-louis.franques@epitech.eu>
2019-10-06 19:34:13 +0200

    Filtre noir et blanc progressif dans zone de danger

--- GD/Assets/Common/Script/DangerZone.cs
+++ GD/Assets/Common/Script/DangerZone.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 
 public class DangerZone : MonoBehaviour
 {
@@ -8,10 +9,18 @@ public class DangerZone : MonoBehaviour
     float _startPosZ;
     float _distance;
     float _duration;
+    float _duration2;
     int _chanceToDie;
     int _pourcentageToDie;
+    float _tmpSaturation;
+    ColorGrading _color;
+    PostProcessVolume _postProcess;
+
+    public GameObject _gameObjPost;
+    //public PostProcessVolume _postProcess;
     public int _rayon;
     public dayNight _dayNight;
+
    
     // Start is called before the first frame update
     void Start()
@@ -19,9 +28,16 @@ public class DangerZone : MonoBehaviour
         _startPosX = transform.localPosition.x;
         _startPosZ = transform.localPosition.z;
         _duration = 2.0f;
+        _duration2 = 0.5f;
         _chanceToDie = 0;
+        _tmpSaturation = 10f;
+        _color = ScriptableObject.CreateInstance<ColorGrading>();
+        _color.enabled.Override(true);
+        _color.saturation.Override(_tmpSaturation);
+        _postProcess = PostProcessManager.instance.QuickVolume(_gameObjPost.layer, 100f, _color);
+
         //if we are on the map number X, the value change 
-        _pourcentageToDie = 5;
+        _pourcentageToDie = 0;
     }
 
     // Update is called once per frame
@@ -31,6 +47,7 @@ public class DangerZone : MonoBehaviour
         if (_distance > _rayon)
         {
             _duration -= Time.deltaTime;
+            _duration2 -= Time.deltaTime;
 
             if (_duration <= 0.0f)
             {
@@ -41,9 +58,31 @@ public class DangerZone : MonoBehaviour
                 _duration = 2.0f;
                 _chanceToDie += _pourcentageToDie;
             }
+
+            if (_duration2 <= 0.0f)
+            {
+                _duration2 = 0.5f;
+                if (_tmpSaturation > -50)
+                {
+                    _tmpSaturation -= 1;
+                    _color.saturation.value = _tmpSaturation;
+                }
+            }
+            
         }
         else
         {
+            _duration2 -= Time.deltaTime;
+
+            if (_duration2 <= 0.0f)
+            {
+                _duration2 = 0.5f;
+                if (_tmpSaturation < 10)
+                {
+                    _tmpSaturation += 1;
+                    _color.saturation.value = _tmpSaturation;
+                }
+            }
             _chanceToDie = 0;
         }
     }
